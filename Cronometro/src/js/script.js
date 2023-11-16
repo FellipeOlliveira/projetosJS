d14077813707e248d0aad62aad6b54fb8ea56506
Marcos <marcosf1625@gmail.com>
2023-11-16 10:07:42 -0300

    commit final, tudo concluido e o projeto esta funcionando perfeitamente

--- Cronometro/src/js/script.js
+++ Cronometro/src/js/script.js
@@ -1,22 +1,50 @@
+//variaveis do programa
 const horas = document.getElementById("hrs")
 const minutos = document.getElementById("min")
 const segundos = document.getElementById("sec")
 var hrs = 0
 var min = 0
 var sec = 0
+var milisec = 0
 var contador = 0
 
-function increment(){
-    let valores = valor
-    contador ++
-    valores.innerHTML = contador
-    print("deu certo")
+//inicializando o contador
+function ignition(){
+    breaks()
+    contador = setInterval(() => {deterninarHora(), 10})    
 }
-//Stts: Concluido
+
+//encontrando a hora, logica do calculo do tempo
+function deterninarHora(){
+    if ((milisec += 10 ) == 1000){
+        milisec = 0
+        sec ++
+    }
+    if (sec == 60){
+        sec = 0
+        min ++
+    }
+    if (min == 60){
+        min = 0
+        hrs ++
+    }
+    horas.innerHTML = hrs
+    minutos.innerHTML = min
+    segundos.innerHTML = sec
+
+}
+
+//Para parar a função
+function breaks(){
+    clearInterval(contador)
+}
+
+//Revertendo as variaveis para o inicio
 function reverb(){
     hrs = 0
     min = 0
     sec = 0
+    milisec = 0
     contador = 0
 
     horas.innerHTML = hrs
